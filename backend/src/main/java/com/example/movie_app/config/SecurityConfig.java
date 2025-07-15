26118612b0d337c706d74d70e7879c833735f56a
Willy Saronamihardja <wsaronam@gmail.com>
2025-07-15 01:00:31 -0700

    small updates
    - added security stuff to program
    - debugging spring boot issues (WIP)

---
+++ backend/src/main/java/com/example/movie_app/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.movie_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll() // allows users to sign up or login without auth
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+}
